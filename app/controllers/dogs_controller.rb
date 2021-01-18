ac938b27b2be0582a3bd647e6351c071697a2296
shmuly11 <shmuly11@gmail.com>
2021-01-18 15:49:23 -0500

    completed

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,35 @@
 class DogsController < ApplicationController
+    before_action :set_dog, only: [:edit, :show, :update, :destroy]
+    
+    def index
+        @dogs = Dog.all
+    end
+
+    def new
+        @dog = Dog.new
+    end
+
+    def create
+    end
+
+    def show
+        
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+
+    def set_dog
+        @dog = Dog.find(params[:id])
+    end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,22 @@
 Rails.application.routes.draw do
-  resources :dogs
-  resources :employees
+  # resources :dogs
+  # resources :employees
+
+  get '/dogs', to: 'dogs#index', as: 'dogs'
+  get '/dogs/new', to: 'dogs#new', as: 'new_dog'
+  post '/dogs', to: 'dogs#create'
+  get '/dogs/:id', to: 'dogs#show', as: 'dog'
+  get '/dogs/:id/edit', to: 'dogs#edit', as: 'edit_dog'
+  patch '/dogs/:id', to: 'dogs#update'
+  delete '/dogs/:id', to: 'dogs#destroy'
+
+  get '/employees', to: 'employees#index', as: 'employees'
+  get '/employees/new', to: 'employees#new', as: 'new_employee'
+  post '/employees', to: 'employees#create'
+  get '/employees/:id', to: 'employees#show', as: 'employee'
+  get '/employees/:id/edit', to: 'employees#edit', as: 'edit_employee'
+  patch '/employees/:id', to: 'employees#update'
+  delete '/employees/:id', to: 'employees#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
