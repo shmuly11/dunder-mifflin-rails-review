dd5815566ebba50c7fa3e30dfdf3cfd58fdc7e05
shmuly11 <shmuly11@gmail.com>
2021-01-18 15:10:42 -0500

    commit tree a41453a616c1b8c8777f281295823e89777f501a

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,55 @@
 class EmployeesController < ApplicationController
+    before_action :set_employee, only: [:edit, :show, :update, :destroy]
+    
+    def index
+        @employees = Employee.all
+    end
+
+    def new
+        @employee = Employee.new
+    end
+
+    def create
+        
+        new_employee = Employee.create(employee_info(:first_name, :last_name, :alias, :title, :office, :dog_id))
+        if new_employee.valid?
+            redirect_to employee_path(new_employee)
+        else
+            flash[:messages] = new_employee.errors.full_messages
+            redirect_to new_employee_path
+        end
+
+    end
+
+    def show
+        
+    end
+
+    def edit
+    end
+
+    def update
+       @employee.update(employee_info(:first_name, :last_name, :alias, :title, :office, :dog_id))
+       if @employee.valid?
+        redirect_to employee_path(@employee)
+       else
+        flash[:messages] = @employee.errors.full_messages
+            render :edit
+       end
+
+
+    end
+
+    def destroy
+    end
+
+    private
+
+    def set_employee
+        @employee = Employee.find(params[:id])
+    end
+
+    def employee_info(*args)
+        params.require(:employee).permit(*args)
+    end
 end
